2c6b2d7ef8a4f0b375b7400c17243ff8a923c3bd
raazkumar24 <raazkumar625@gmail.com>
2026-03-06 09:41:56 +0530

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { inspectAttr } from 'kimi-plugin-inspect-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
